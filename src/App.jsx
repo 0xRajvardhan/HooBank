83be6316a09626bf5407e131732dda9e33e7a39e
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-25 23:12:33 +0530

    :sparkles: added: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import styles from './style';
 
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
